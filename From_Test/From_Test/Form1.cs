50e0c0d7023ddfae6a12ffd98c2594e5576fa64a
FMVA53E3G\User <cait.sith.0520@gmail.com>
2025-10-31 14:35:09 +0900

    ここからライツアウトスタート

--- From_Test/From_Test/Form1.cs
+++ From_Test/From_Test/Form1.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace From_Test
@@ -20,46 +14,33 @@ namespace From_Test
             //模範解答では、iとjを位置設定の値に掛けてた
             // TestButton.Location = new Point(50 * i,50 * j);
 
-            int tate = 20;
-            for (int j = 0; j < 6; j++)
+     
+            for (int j = 0; j < 3; j++)
             {
-                int iti = 50;
 
-                for (int i = 0; i < 4; i++)
+
+                for (int i = 0; i < 3; i++)
                 {
 
                     //インスタンスの生成
-                    TestButton testButton = new TestButton();
+                    TestButton testButton = new TestButton(new Point(50 * i, 50 * j), new Size(50, 50), "あいうえお");
 
-                    //ボタンのサイズを設定
-                    testButton.Size = new Size(50, 50);
 
-                    //ボタンの位置を設定
-                    testButton.Location = new Point(iti, tate);
 
-                    //ボタン内のテキストを設定
-                    testButton.Text = "あいうえお";
-
-                    //マウスホバーなるものもあるらしい
-                    testButton.Click += hogehogeClick;
 
 
                     //コントロールのボタンを設置
                     Controls.Add(testButton);
 
-                    iti += 50;
+               
                 }
 
-             tate += 50;
 
             }
-            
+
         }
-        //自分で作成することも可能
-        private void hogehogeClick(object sender, EventArgs e)
-        {
-            MessageBox.Show("人体模型");
-        }
+
+
 
 
         private void button1_Click(object sender, EventArgs e)
--- From_Test/From_Test/TestButton.cs
+++ From_Test/From_Test/TestButton.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,5 +12,20 @@ namespace From_Test
     //TestButtonクラスを作成してください。
     internal class TestButton : Button
     {
+        public TestButton(Point position,Size size, string test)
+        { 
+            Location = position;
+            Size = size;
+            Text = test;
+
+
+            Click += hogehogeClick;
+
+        }
+
+        private void hogehogeClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("人体模型");
+        }
     }
 }
